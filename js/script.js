e8cf85a60aa1409878eea8124949c9760e699d83
Chery <cherylene40@gmail.com>
2024-03-25 09:30:28 +0700

    add jquery tab

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.tabs-4').jQueryTab({
+    "responsive": "yes",
+    "collapsible": "no",
+    "usecookie": "yes",
+    "event": "hover",
+    "transition": "slideUp",
+    "callback functions": "none",
+    "initialtab": 3,
+});
